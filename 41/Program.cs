8dfa2bdbb5ba60ae698a3f0d7ccf811bfda74889
fosterjok <fosterjok@mail.ru>
2022-10-21 15:13:02 +0300

    41

---
+++ 41/Program.cs
@@ -0,0 +1,18 @@
+﻿ /*Пользователь вводит с клавиатуры M чисел. 
+ Посчитайте, сколько чисел больше 0 ввёл пользователь.*/
+
+int [] array= new int[5];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i]= new Random ().Next(-10,10);
+    Console.Write($"{array[i]} ");}
+       int count=0;
+   for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Кол-во элементов > 0: {count}");
+    
